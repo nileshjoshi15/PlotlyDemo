9be4132f781ba2d159b3976073fb0105068a0de2
njsohi <nileshjoshi.bn@gmail.com>
2018-06-12 23:45:05 -0500

    plotlyDemo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Plotly from 'plotly.js/dist/plotly.js';
+import {Config, Data, Layout} from 'plotly.js/dist/plotly.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor() {
+  }
+
+  ngOnInit() {
+    let y0 = [];
+    let y1 = [];
+    for (var i = 0; i < 50; i++) {
+      y0[i] = Math.random();
+      y1[i] = Math.random() + 1;
+    }
+    var trace1 = {
+      y: y0,
+      type: 'box'
+    };
+    let trace2 = {
+      y: y1,
+      type: 'box'
+    };
+
+    let data = [trace1, trace2];
+    Plotly.newPlot('myDiv', data);
+  }
 }
